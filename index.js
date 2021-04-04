f58fec3fe8a95db5fa049282685af3246023137a
vivek286 <solankivivek01@gmail.com>
2021-04-04 17:55:05 +0530

    Added login page

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const { response } = require('express');
-const express =require('express');
+const express=require('express');
 const path=require('path');
-const port=8000;
+const port =8000;
+const app=express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.get('/',function(req,res){
+   return res.render('login_signup');
+})
+
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log('error in running server',err);
+    }
+    console.log('server isrunning on',port);
+})
